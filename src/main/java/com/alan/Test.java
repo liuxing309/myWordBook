e5002597e1a7a719b3859aeb29ed7e0d16783eba
Eckyoark <1060884431@qq.com>
2019-04-11 12:01:36 +0800

    来自付的修改

--- src/main/java/com/alan/Test.java
+++ src/main/java/com/alan/Test.java
@@ -3,4 +3,7 @@ package com.alan;
 public class Test {
     public void demo1(){}
 
+    public static void main(String[] args) {
+        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
+    }
 }
